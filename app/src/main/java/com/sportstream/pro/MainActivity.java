819864d9e3083f46bf20089618eedac45f18a95e
dalibordizajn-source <dalibor.dizajn@gmail.com>
2026-04-25 11:54:10 +0200

    Create MainActivity.java

---
+++ app/src/main/java/com/sportstream/pro/MainActivity.java
@@ -0,0 +1,43 @@
+package com.sportstream.pro;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView overlayWebView;
+    private boolean isBlackout = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        overlayWebView = findViewById(R.id.overlay_webview);
+        WebSettings webSettings = overlayWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        overlayWebView.setBackgroundColor(0); 
+        overlayWebView.setWebViewClient(new WebViewClient());
+        
+        overlayWebView.loadUrl("about:blank");
+
+        Button btnBlackout = findViewById(R.id.btn_blackout);
+        btnBlackout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isBlackout) {
+                    overlayWebView.setVisibility(View.INVISIBLE);
+                    isBlackout = true;
+                } else {
+                    overlayWebView.setVisibility(View.VISIBLE);
+                    isBlackout = false;
+                }
+            }
+        });
+    }
+}
